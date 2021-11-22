57b553d693782ffbd328da694a3d14feb0fdf0bb
Serg <a_s_v86@mail.ru>
2021-11-22 21:59:15 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Reflection;
+using test;
+namespace test
+{
+    
+   public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите первое число");
+                string FirstValue = Console.ReadLine();
+                Console.WriteLine("Введите знак операции");
+                string Sign = Console.ReadLine();
+                Console.WriteLine("Введите второе число");
+                string SecondValue = Console.ReadLine();
+
+                UserData user = new UserData(FirstValue, Sign, SecondValue);
+
+                var builder = new ContainerBuilder();
+                builder.RegisterType<ContainerCreator>().OnActivating(e => e.Instance.CreateContainer(user));
+                IContainer container = builder.Build();
+                container.Resolve<ContainerCreator>();
+                Console.ReadKey();
+            }
+
+        }
+    }
+    interface IOperation
+    {
+        void Operation(double a, double b);
+    }
+    class Sum : IOperation
+    {
+        public void Operation(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+
+    class Minus : IOperation
+    {
+        public void Operation(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+    }
+
+    class Multiply : IOperation
+    {
+        public void Operation(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+    }
+
+    class Divide : IOperation
+    {
+        public void Operation(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            Console.WriteLine(a / b);
+        }
+    }
+
+    class BusinessLogic
+    {
+        public void DoOperation(IOperation Operations, UserData userData)
+        {
+            Operations.Operation(userData.FirstValue, userData.SecondValue);
+        }
+    }
+    interface IOtherOperation
+    {
+        void Operation(double a, double b);
+    }
+    class Interpolation : IOtherOperation
+    {
+        public void Operation( double a, double b)
+        {
+            Console.WriteLine(Math.Pow(a,b));
+        }
+    }
+    class InterpolationAdapter : IOperation
+    {
+        Interpolation interpolation;
+        public InterpolationAdapter(Interpolation inter)
+        {
+            interpolation = inter;
+        }
+
+        public void Operation(double a, double b)
+        {
+            interpolation.Operation(a,b);
+        }
+    }
+    public class ContainerCreator
+    {
+        public interface IContainerCreator
+        {
+            void CreateContainer(UserData user);
+        }
+        private static IContainer Container { get; set; }
+        public void CreateContainer(UserData user)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Sum>().OnActivating(e => e.Instance.Operation(user.FirstValue, user.SecondValue));
+            builder.RegisterType<Minus>().OnActivating(e => e.Instance.Operation(user.FirstValue, user.SecondValue));
+            builder.RegisterType<Multiply>().OnActivating(e => e.Instance.Operation(user.FirstValue, user.SecondValue));
+            builder.RegisterType<Divide>().OnActivating(e => e.Instance.Operation(user.FirstValue, user.SecondValue));
+            builder.RegisterType<Interpolation>().OnActivating(e => e.Instance.Operation(user.FirstValue, user.SecondValue));
+            Container = builder.Build();
+
+            switch (user.OperationSign)
+            {
+                case "+":
+                    Container.Resolve<Sum>();
+                    break;
+                case "-":
+                    Container.Resolve<Minus>();
+                    break;
+                case "*":
+                    Container.Resolve<Multiply>();
+                    break;
+                case "/":
+                    Container.Resolve<Divide>();
+                    break;
+                case ">>":
+                    Container.Resolve<Interpolation>();
+                    break;
+            }
+
+        }
+    }
+}
---
+++ UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class UserData
+    {
+        public double FirstValue { get; set; }
+        public string OperationSign { get; set; }
+        public double SecondValue { get; set; }
+        public UserData(string _FirstValue, string _Sign, string _SecondValues)
+        {
+            FirstValue = Convert.ToDouble(_FirstValue);
+            OperationSign = _Sign;
+            SecondValue = Convert.ToDouble(_SecondValues);
+        }
+
+
+
+    }
+}
